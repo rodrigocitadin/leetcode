9c1c426f2a051e3bacdc07a35db9f6fc410c6a2f
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-03 23:18:08 -0300

    back with leetcode

---
+++ sqrt_x/sqrt_x.ts
@@ -0,0 +1,10 @@
+function mySqrt(x: number): number {
+    let result = x;
+    let precision = 0.00001;
+
+    while (Math.abs(x - result * result) > precision) {
+        result = (result + x / result) / 2
+    }
+
+    return Math.floor(result)
+};
